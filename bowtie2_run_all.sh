34388ac0b53a9860951514f082c8a5f8809d2b99
Samantha Hao <s.hao@berkeley.edu>
2018-06-25 14:23:47 -0700

    fixed bowtie2 sequential script

--- bowtie2_run_all.sh
+++ bowtie2_run_all.sh
@@ -3,9 +3,9 @@ for file1 in *fastq
 do
 infolder=${file1%%.fastq}_output
 index=${file1%%.fastq}
-# builds index multi-threaded
-bowtie2-build --threads 32 /mnt/data/$infolder/contigs.fasta $index
+# builds index
+bowtie2-build /mnt/data/spades_out/$infolder/contigs.fasta $index
 out=${file1%%.fastq}_align.sam
 # runs bowtie2 with timer, metrics, multi-threaded, reads that failed to align
-bowtie2 -t --met-file /mnt/data/$index_metrics.txt -p 32 --un /mnt/data/$index_failedreads.fasta -x $index -U $file1 -S $out
+bowtie2 -t --met-file /mnt/data/${index}_metrics.txt -p 32 --un /mnt/data/${index}_failedreads.fasta -x $index -U $file1 -S $out
 done
