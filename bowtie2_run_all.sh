18ff515a867d45eaed82f27b2aeadf9d01584eb3
Samantha Hao <s.hao@berkeley.edu>
2018-06-25 17:05:47 -0700

    more generic script for running bowtie2 on many fastq files

--- bowtie2_run_all.sh
+++ bowtie2_run_all.sh
@@ -1,11 +1,15 @@
+#!/usr/bin/env bash
+# usage: bowtie2_run_all.sh <full path to reference file> <full path to output folder>
 # builds bowtie2 index and runs bowtie2 on all fastq files in the directory
 for file1 in *fastq
 do
-infolder=${file1%%.fastq}_output
+path_to_contigs=${1?Error: path to reference file not given}
+# infolder=${file1%%.fastq}_output
 index=${file1%%.fastq}
-# builds index
-bowtie2-build /mnt/data/spades_out/$infolder/contigs.fasta $index
+# builds index from spades output
+bowtie2-build $path_to_contigs $index
+out_folder=${2?Error: output folder not specified}
 out=${file1%%.fastq}_align.sam
 # runs bowtie2 with timer, metrics, multi-threaded, reads that failed to align
-bowtie2 -t --met-file /mnt/data/${index}_metrics.txt -p 32 --un /mnt/data/${index}_failedreads.fasta -x $index -U $file1 -S $out
+bowtie2 -t --met-file $out_folder/${index}_metrics.txt -p 32 --un $out_folder/${index}_failedreads.fasta -x $index -U $file1 -S $out_folder/$out
 done
